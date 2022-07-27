8fc6a2ad8cfeb3f90a9ec80a5f6e1d26f19c210e
Matt Williams <matt.seongjun@gmail.com>
2022-07-26 22:55:56 -0400

    Add secure passwords

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "user_#{n}" }
     sequence(:email) { |n| "user_#{n}@example.test" }
+    password { "password" }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,12 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   it "has a valid factory" do
-    expect(create(:user)).to be_valid
+    expect(build(:user)).to be_valid
+  end
+
+  context "when password does not match confirmation" do
+    it "is invalid" do
+      expect(build(:user, password_confirmation: "wrong")).to_not be_valid
+    end
   end
 end
