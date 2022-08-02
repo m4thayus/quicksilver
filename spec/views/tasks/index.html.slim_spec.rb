009ff4e2873970d5dd24257f1016821b288933f0
Matt Williams <matt.seongjun@gmail.com>
2022-08-02 09:11:25 -0400

    Flesh out index

--- spec/views/tasks/index.html.slim_spec.rb
+++ spec/views/tasks/index.html.slim_spec.rb
@@ -2,6 +2,40 @@
 
 require "rails_helper"
 
-RSpec.describe "tasks/index.html.slim", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe "tasks/index", type: :view do
+  let(:tasks) do
+    [0..2].map do
+      create(:task) do |t|
+        create(:assignee, task: t)
+      end
+    end
+  end
+
+  before do
+    assign(:tasks, tasks)
+  end
+
+  describe "the first task" do
+    it "renders the title" do
+      render
+      expect(rendered).to match(/#{tasks.first.title}/)
+    end
+
+    it "renders the assignee" do
+      render
+      expect(rendered).to match(/#{tasks.first.assignees.first.user.name}/)
+    end
+  end
+
+  describe "the last task" do
+    it "renders the title" do
+      render
+      expect(rendered).to match(/#{tasks.last.title}/)
+    end
+
+    it "renders the assignee" do
+      render
+      expect(rendered).to match(/#{tasks.last.assignees.first.user.name}/)
+    end
+  end
 end
