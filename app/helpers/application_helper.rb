cff072759a20779cb83a1cccb2645567829c7190
Scott Brickner <scottb@brickner.net>
2022-08-11 15:06:22 -0400

    Add markdown support for task description (#7)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def markdown
+    @markdown ||= begin
+                    renderer = Redcarpet::Render::HTML.new(link_attributes: { target: "_blank" })
+                    Redcarpet::Markdown.new(renderer, no_intra_emphasis: true, autolink: true, underline: true)
+                  end
+  end
 end
