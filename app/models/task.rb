ad2183aa1fd8c486ab778c18ac38ba0c7cc6ac6a
Matt Williams <matt.seongjun@gmail.com>
2022-08-20 09:07:45 -0400

    Add scopes for filtering tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,7 @@ class Task < ApplicationRecord
   belongs_to :owner, class_name: "User", optional: true
 
   validates :title, presence: true
+
+  scope :active, -> { where(completed_at: nil) }
+  scope :recently_completed, -> { where(completed_at: 1.week.ago..) }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Task, type: :model do
   it "can be owned by user" do
     expect(build(:task, owner: create(:user))).to be_valid
   end
+
+  it "has an active scope" do
+    create(:task, completed_at: nil)
+    expect(described_class.active.length).to eq(1)
+  end
+
+  it "has a recently completed at scope" do
+    create(:task, completed_at: 3.days.ago)
+    expect(described_class.recently_completed.length).to eq(1)
+  end
 end
