3b30f2c047818ef9044e128a70094fcfbd8d5a69
Scott Brickner <scottb@brickner.net>
2022-09-06 17:54:17 -0400

    Change markdown options to match EasyMDE (#12)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,14 @@
 module ApplicationHelper
   def markdown
     @markdown ||= begin
-                    renderer = Redcarpet::Render::HTML.new(link_attributes: { target: "_blank" })
-                    Redcarpet::Markdown.new(renderer, no_intra_emphasis: true, autolink: true, underline: true)
+                    opts = optionize(%i[with_toc_data hard_wrap gh_blockcode])
+                    opts[:link_attributes] = { target: "_blank" }
+                    renderer = Redcarpet::Render::HTML.new(opts)
+                    Redcarpet::Markdown.new(renderer, optionize(%i[no_intra_emphasis autolink underline fenced_code_blocks tables strikethrough space_after_headers]))
                   end
+  end
+
+  def optionize(options)
+    options.each_with_object({}) { |option, memo| memo[option] = true }
   end
 end
