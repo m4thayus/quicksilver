a99c0911cfe0ee69f04ebec1528247b85ebcae65
zoeblairfriedman <zoeblair.friedman@gmail.com>
2022-09-30 10:14:24 -0400

    Add specs for promotion of wishlist task

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,8 @@ class User < ApplicationRecord
 
   ADMINISTRATORS = %w[scottb mattw].map { |u| "#{u}@mercuryanalytics.com" }.freeze
   ENGINEERS = %w[peterv deepthie elizabethk jamesa zoef biancac amitp].map { |u| "#{u}@mercuryanalytics.com" }.freeze
+  # ENGINEERS = %w[peterv deepthie elizabethk jamesa biancac amitp].map { |u| "#{u}@mercuryanalytics.com" }.freeze
+  # MEMBERS = %w[ronh jail zoef].map { |u| "#{u}@mercuryanalytics.com" }.freeze
   MEMBERS = %w[ronh jail].map { |u| "#{u}@mercuryanalytics.com" }.freeze
 
   def admin?
--- spec/features/board_spec.rb
+++ spec/features/board_spec.rb
@@ -1,12 +1,19 @@
 # frozen_string_literal: true
 
+require "rails_helper"
+
 RSpec.describe Board, type: :feature do
+  let(:wishlist_board) { create(:wishlist) }
+
+  before do
+    create(:task, board: wishlist_board)
+  end
+
   context "when allowed to promote" do
     let(:wishlist_board) { create(:wishlist) }
 
     before do
       login_as :engineer_user
-      create(:task, board: wishlist_board)
       visit "/boards/#{wishlist_board.name}/tasks"
     end
 
@@ -15,9 +22,40 @@ RSpec.describe Board, type: :feature do
     end
 
     context "when promote button is clicked" do
-      xit "clears board association"
-      xit "navigates to task list"
-      xit "promoted task now appears on the task board"
+      let(:task_to_approve) { create(:task, title: "My New Task") }
+
+      before do
+        task_to_approve.approved = true
+      end
+
+      it "clears board association" do
+        click_button "Promote"
+
+        expect(task_to_approve.approved).to be_nil
+      end
+
+      it "navigates to task list" do
+        click_button "Promote"
+
+        expect(page).to have_current_path(tasks_path)
+      end
+
+      it "promoted task now appears on the task board" do
+        click_button "Promote"
+
+        expect(page).to have_text("My New Task")
+      end
+    end
+  end
+
+  context "when not allowed to promote" do
+    before do
+      login_as :member_user
+      visit "/boards/#{wishlist_board.name}/tasks"
+    end
+
+    it "shows buttons for promotion of wishlist" do
+      expect(page).to_not have_button "Promote"
     end
   end
 end
