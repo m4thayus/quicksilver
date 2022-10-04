81cca784be7468a8b210ebba097d845136f62f63
zoeblairfriedman <zoeblair.friedman@gmail.com>
2022-10-04 14:41:09 -0400

    Add feature specs for wishlist

--- spec/features/task_list_spec.rb
+++ spec/features/task_list_spec.rb
@@ -5,32 +5,56 @@ require "rails_helper"
 RSpec.describe "Task List", type: :feature do
   before do
     login_as :engineer_user
-
-    create_list(:task, 3, completed_at: nil)
-    create_list(:task, 3, completed_at: 3.days.ago)
   end
 
-  it "shows all the tasks" do
-    visit tasks_path
+  context "when visiting index of tasks without board" do
+    before do
+      create_list(:task, 3, completed_at: nil)
+      create_list(:task, 3, completed_at: 3.days.ago)
+    end
 
-    expect(page.all("tr").size).to eq 6
+    it "shows all the tasks" do
+      visit tasks_path
+
+      expect(page.all("tr").size).to eq 6
+    end
+
+    it "shows the active tasks" do
+      visit tasks_path
+
+      expect(page.all("table:first-of-type tr").size).to eq 3
+    end
+
+    it "shows the recently completed tasks" do
+      visit tasks_path
+
+      expect(page.all("table:last-of-type tr").size).to eq 3
+    end
+
+    it "shows a column for reviewed tasks" do
+      visit tasks_path
+
+      expect(page).to have_text "Reviewed"
+    end
   end
 
-  it "shows the active tasks" do
-    visit tasks_path
+  context "when visiting wishlist board" do
+    let(:wishlist) { create(:wishlist) }
 
-    expect(page.all("table:first-of-type tr").size).to eq 3
-  end
+    before do
+      create_list(:task, 3, board: wishlist)
+    end
 
-  it "shows the recently completed tasks" do
-    visit tasks_path
+    it "shows all the wishlist tasks" do
+      visit board_tasks_path(wishlist)
 
-    expect(page.all("table:last-of-type tr").size).to eq 3
-  end
+      expect(page.all("tr").size).to eq 3
+    end
 
-  it "shows a column for approved" do
-    visit tasks_path
+    it "shows a column for approved tasks" do
+      visit board_tasks_path(wishlist)
 
-    expect(page).to have_text "Reviewed"
+      expect(page).to have_text "Approved"
+    end
   end
 end
