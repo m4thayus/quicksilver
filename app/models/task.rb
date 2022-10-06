f94713c7d9f239295d1c8c469af59677fd2e7a5a
zoeblairfriedman <zoeblair.friedman@gmail.com>
2022-10-06 11:57:15 -0400

    Fix rubocop error

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,13 +2,13 @@
 
 class Task < ApplicationRecord
   SIZES = %w[small medium large].freeze
-  before_validation :nillify_size
 
   belongs_to :board, optional: true
   belongs_to :owner, class_name: "User", optional: true
 
   validates :title, presence: true
   validates :size, inclusion: { in: SIZES }, allow_nil: true
+  before_validation :nillify_size
 
   scope :active, -> { where(completed_at: nil) }
   scope :recently_completed, -> { where("completed_at > ?", 1.week.ago) }
@@ -16,6 +16,6 @@ class Task < ApplicationRecord
   private
 
   def nillify_size
-    self.size = nil if size.empty?
+    self.size = nil if size&.empty? # rubocop:disable Lint/SafeNavigationWithEmpty
   end
 end
