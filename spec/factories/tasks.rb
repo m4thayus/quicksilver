6d1092318e339fb260016393c4092cc5871773b7
Matt Williams <matt.seongjun@gmail.com>
2022-12-06 11:23:18 -0500

    Update factory

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,9 +3,12 @@
 FactoryBot.define do
   factory :task do
     title { "Title" }
+    status { "Status Here" }
     description { "Description" }
     started_at { 1.month.ago }
     expected_at { 2.weeks.ago }
     completed_at { 2.hours.ago }
+    point_estimate { 5 }
+    points { 8 }
   end
 end
