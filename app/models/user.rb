89a3897349f9d78db031bc2eb503b6b950c5e5a7
Matt Williams <matt.seongjun@gmail.com>
2025-03-04 16:48:53 -0500

    feat: replace with in group?

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,23 +14,29 @@ class User < ApplicationRecord
   BUISNESS_DEVELOPERS = %w[jordank leighk].map { |u| "#{u}@mercuryanalytics.com" }.freeze
   GUESTS = %w[andrewg praneetp prasadd aishwaryap anastasiar jillianh brandonk shardulm].map { |u| "#{u}@mercuryanalytics.com" }.freeze
 
+  def in_group?(group_slug)
+    case group_slug
+    when :admin
+      ADMINISTRATORS.include?(email)
+    when :engineer
+      ENGINEERS.include?(email)
+    when :member
+      MEMBERS.include?(email)
+    when :bizdev
+      BUISNESS_DEVELOPERS.include?(email)
+    when :guest
+      GUESTS.include?(email)
+    else
+      false
+    end
+  end
+
+  # TODO: Remove these two methods after board relationship redesign
   def admin?
-    ADMINISTRATORS.include?(email)
+    in_group?(:admin)
   end
 
   def engineer?
-    ENGINEERS.include?(email)
-  end
-
-  def member?
-    MEMBERS.include?(email)
-  end
-
-  def bizdev?
-    BUISNESS_DEVELOPERS.include?(email)
-  end
-
-  def guest?
-    GUESTS.include?(email)
+    in_group?(:engineer)
   end
 end
