47bc44f731db0b8a2b7c31c8962d8d683d36a5ec
Matt Williams <48694013+m4thayus@users.noreply.github.com>
2025-03-04 17:20:56 -0500

    feat: add bizdev board (#27)
    * feat: add biz dev board and user group
    
    * feat: replace with in group?
    
    * feat: add biz dev board to nav
    
    * feat: add abilities for biz dev and disable certain specs
    
    * chore: remove outdated users

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,32 +9,50 @@ class Ability
     can :search, Task
     can :manage, user
 
-    if user.guest?
+    if user.in_group?(:guest)
       can :read, Board
       can :read, User
       can :read, Task
+
       can :manage, Task, board: Board.suggestions
+
       cannot :update, Task, %i[board_id started_at expected_at completed_at points point_estimate approved]
       cannot :create, Task, %i[board_id started_at expected_at completed_at points point_estimate approved]
     end
 
-    if user.member?
+    if user.in_group?(:bizdev)
       can :read, Board
       can :read, User
       can :read, Task
+
+      cannot :update, Task, %i[board_id started_at expected_at completed_at points point_estimate approved]
+      cannot :create, Task, %i[board_id started_at expected_at completed_at points point_estimate approved]
+
+      can :manage, Task, board: Board.suggestions
+      can :manage, Task, board: Board.bizdev
+    end
+
+    if user.in_group?(:member)
+      can :read, Board
+      can :read, User
+      can :read, Task
+
       can :update, Task, %i[description priority]
       can :manage, Task, board: Board.wishlist
+
       cannot :update, Task, %i[board_id started_at expected_at completed_at points point_estimate]
       cannot :create, Task, %i[board_id started_at expected_at completed_at points point_estimate]
+
+      can :manage, Task, board: Board.bizdev
       can :manage, Task, board: Board.suggestions
     end
 
-    if user.engineer?
+    if user.in_group?(:engineer)
       can :read, :all
       can :manage, Task
       can :manage, Board
     end
 
-    can :manage, :all if user.admin?
+    can :manage, :all if user.in_group?(:admin)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,23 +9,34 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
 
   ADMINISTRATORS = %w[scottb mattw].map { |u| "#{u}@mercuryanalytics.com" }.freeze
-  ENGINEERS = %w[peterv deepthie elizabethk jamesa zoef biancac amitp].map { |u| "#{u}@mercuryanalytics.com" }.freeze
-  MEMBERS = %w[ronh jail].map { |u| "#{u}@mercuryanalytics.com" }.freeze
-  GUESTS = %w[andrewg praneetp prasadd aishwaryap jordank anastasiar jillianh brandonk shardulm].map { |u| "#{u}@mercuryanalytics.com" }.freeze
+  ENGINEERS = %w[peterv elizabethk jamesa zoef biancac amitp].map { |u| "#{u}@mercuryanalytics.com" }.freeze
+  MEMBERS = %w[ronh].map { |u| "#{u}@mercuryanalytics.com" }.freeze
+  BUISNESS_DEVELOPERS = %w[jordank leighk].map { |u| "#{u}@mercuryanalytics.com" }.freeze
+  GUESTS = %w[andrewg praneetp prasadd aishwaryap anastasiar jillianh brandonk shardulm].map { |u| "#{u}@mercuryanalytics.com" }.freeze
 
+  def in_group?(group_slug)
+    case group_slug
+    when :admin
+      ADMINISTRATORS.include?(email)
+    when :engineer
+      ENGINEERS.include?(email)
+    when :member
+      MEMBERS.include?(email)
+    when :bizdev
+      BUISNESS_DEVELOPERS.include?(email)
+    when :guest
+      GUESTS.include?(email)
+    else
+      false
+    end
+  end
+
+  # TODO: Remove these two methods after board relationship redesign
   def admin?
-    ADMINISTRATORS.include?(email)
+    in_group?(:admin)
   end
 
   def engineer?
-    ENGINEERS.include?(email) || admin?
-  end
-
-  def member?
-    MEMBERS.include?(email) || engineer?
-  end
-
-  def guest?
-    GUESTS.include?(email)
+    in_group?(:engineer)
   end
 end
